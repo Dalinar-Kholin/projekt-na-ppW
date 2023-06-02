6c674d40d8f0d6de2d3f5721eace087a7f07a06a
DalinarKholin <Osadowskikacper@gmail.com>
2023-06-02 18:47:20 +0200

    dodanie działającego drzewa wyboru folderu - na razie działa tylko dla jedego dysku

--- projekt na ppW/Form1.Designer.cs
+++ projekt na ppW/Form1.Designer.cs
@@ -30,20 +30,18 @@
         {
             treeView1 = new TreeView();
             listView1 = new ListView();
-            addElem = new Button();
-            delElem = new Button();
-            edytuj = new Button();
-            button5 = new Button();
-            textBox1 = new TextBox();
             richTextBox1 = new RichTextBox();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            backgroundWorker3 = new System.ComponentModel.BackgroundWorker();
             SuspendLayout();
             // 
             // treeView1
             // 
             treeView1.Anchor = AnchorStyles.Top;
-            treeView1.Location = new Point(12, 12);
+            treeView1.Location = new Point(73, 12);
             treeView1.Name = "treeView1";
-            treeView1.Size = new Size(146, 539);
+            treeView1.Size = new Size(423, 539);
             treeView1.TabIndex = 0;
             treeView1.AfterSelect += treeView1_AfterSelect;
             // 
@@ -54,7 +52,7 @@
             listView1.Anchor = AnchorStyles.Top;
             listView1.ImeMode = ImeMode.On;
             listView1.LabelEdit = true;
-            listView1.Location = new Point(164, 12);
+            listView1.Location = new Point(526, 12);
             listView1.MultiSelect = false;
             listView1.Name = "listView1";
             listView1.Size = new Size(317, 539);
@@ -65,92 +63,36 @@
             listView1.SelectedIndexChanged += listView1_SelectedIndexChanged;
             listView1.DoubleClick += listView1_DoubleClick;
             // 
-            // addElem
-            // 
-            addElem.Anchor = AnchorStyles.Top;
-            addElem.Location = new Point(487, 12);
-            addElem.Name = "addElem";
-            addElem.Size = new Size(419, 103);
-            addElem.TabIndex = 2;
-            addElem.Text = "dodoaj element";
-            addElem.UseVisualStyleBackColor = true;
-            addElem.Click += button1_Click;
-            // 
-            // delElem
-            // 
-            delElem.Anchor = AnchorStyles.Top;
-            delElem.Location = new Point(487, 121);
-            delElem.Name = "delElem";
-            delElem.Size = new Size(419, 103);
-            delElem.TabIndex = 4;
-            delElem.Text = "usuń Element";
-            delElem.UseVisualStyleBackColor = true;
-            // 
-            // edytuj
-            // 
-            edytuj.Anchor = AnchorStyles.Top;
-            edytuj.Location = new Point(487, 230);
-            edytuj.Name = "edytuj";
-            edytuj.Size = new Size(419, 103);
-            edytuj.TabIndex = 5;
-            edytuj.Text = "editr-open";
-            edytuj.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            button5.Anchor = AnchorStyles.Top;
-            button5.Location = new Point(487, 448);
-            button5.Name = "button5";
-            button5.Size = new Size(419, 103);
-            button5.TabIndex = 7;
-            button5.Text = "button5";
-            button5.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(12, 565);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(894, 23);
-            textBox1.TabIndex = 8;
-            textBox1.TextChanged += textBox1_TextChanged;
-            // 
             // richTextBox1
             // 
-            richTextBox1.Location = new Point(487, 339);
+            richTextBox1.Location = new Point(896, 12);
             richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(419, 103);
+            richTextBox1.Size = new Size(419, 539);
             richTextBox1.TabIndex = 9;
             richTextBox1.Text = "";
+            richTextBox1.TextChanged += richTextBox1_TextChanged;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(918, 600);
+            ClientSize = new Size(1352, 607);
             Controls.Add(richTextBox1);
-            Controls.Add(textBox1);
-            Controls.Add(button5);
-            Controls.Add(edytuj);
-            Controls.Add(delElem);
-            Controls.Add(addElem);
             Controls.Add(listView1);
             Controls.Add(treeView1);
             DoubleBuffered = true;
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
         private TreeView treeView1;
         private ListView listView1;
-        private Button addElem;
-        private Button delElem;
-        private Button edytuj;
-        private Button button5;
-        private TextBox textBox1;
         private RichTextBox richTextBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker3;
     }
 }
--- projekt na ppW/Form1.cs
+++ projekt na ppW/Form1.cs
@@ -2,15 +2,38 @@ using System.Diagnostics;
 
 namespace projekt_na_ppW
 {
+
+
+
     public partial class Form1 : Form
     {
-        public string currentFolder { get; set; }
-        public List<string> truePaths { get; set; }
+        private string saver { get; set; }
+        public string currentFolder
+        {
+            get { return saver; }
+            set
+
+            {
+                richTextBox1.Text = value;
+                saver = value;
+                acctListView(this);
+            }
+        }
+        public List<int> listofIndex { get; set; }
+        public List<string> truePathsOfFile { get; set; } = new List<string>();
+        public List<string> truePathsOfDir { get; set; } = new List<string>();
         public Form1()
         {
             InitializeComponent();
             currentFolder = "C:\\";
-            truePaths = new List<string>();
+            var pog = Directory.GetDirectories(currentFolder);
+            treeView1.Nodes.Add("dysk lokalny c ");
+            TreeNode node = treeView1.Nodes[0];
+            foreach (string path in pog)
+            {
+                node.Nodes.Add(path,path.Substring(path.LastIndexOf('\\') + 1));
+                truePathsOfDir.Add(path);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -18,19 +41,17 @@ namespace projekt_na_ppW
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void acctListView(object sender)
         {
             listView1.Items.Clear();
-            truePaths.Clear();
-            currentFolder = textBox1.Text.Trim();
-            button5.Text = currentFolder;
+            truePathsOfFile.Clear();
             try
             {
                 var txtFiles = Directory.EnumerateFiles(currentFolder, "*");
                 foreach (var f in txtFiles)
                 {
-                    listView1.Items.Add(f.Substring(f.LastIndexOf('\\') + 1));
-                    truePaths.Add(f);
+                    listView1.Items.Add(f);
+                    truePathsOfFile.Add(f);
                 }
             }
             catch (Exception ex)
@@ -39,8 +60,34 @@ namespace projekt_na_ppW
             }
         }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
         private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
         {
+
+            truePathsOfFile.Clear();
+            truePathsOfDir.Clear();
+            string path = "";
+            TreeNode actNode = e.Node;
+            currentFolder= actNode.Name;
+            try
+            {
+                var txtFiles = Directory.GetDirectories(currentFolder);
+                foreach (var f in txtFiles)
+                {
+
+                    actNode.Nodes.Add(f,f.Substring(f.LastIndexOf('\\') + 1));
+                    truePathsOfDir.Add(f);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
 
         }
 
@@ -50,12 +97,18 @@ namespace projekt_na_ppW
 
         private void listView1_DoubleClick(object sender, EventArgs e)
         {
-            using (Process proc = new Process()) {
-                proc.StartInfo.FileName = (truePaths[listView1.SelectedItems[0].Index]);
+            using (Process proc = new Process())
+            {
+                proc.StartInfo.FileName = (truePathsOfFile[listView1.SelectedItems[0].Index]);
                 proc.StartInfo.UseShellExecute = false;
                 proc.StartInfo.RedirectStandardOutput = false;
                 proc.Start();
-            } 
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
