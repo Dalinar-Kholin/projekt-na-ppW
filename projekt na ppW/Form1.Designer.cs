f234eaf2d77315855b0ad9d06eef03ed9829e01b
DalinarKholin <Osadowskikacper@gmail.com>
2023-06-02 20:19:07 +0200

    poprawienie logiki i posprzatanie kodu projekt teoretycznie skończony

--- projekt na ppW/Form1.Designer.cs
+++ projekt na ppW/Form1.Designer.cs
@@ -30,7 +30,6 @@
         {
             treeView1 = new TreeView();
             listView1 = new ListView();
-            richTextBox1 = new RichTextBox();
             backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
             backgroundWorker3 = new System.ComponentModel.BackgroundWorker();
@@ -38,8 +37,8 @@
             // 
             // treeView1
             // 
-            treeView1.Anchor = AnchorStyles.Top;
-            treeView1.Location = new Point(73, 12);
+            treeView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            treeView1.Location = new Point(12, 12);
             treeView1.Name = "treeView1";
             treeView1.Size = new Size(423, 539);
             treeView1.TabIndex = 0;
@@ -49,35 +48,26 @@
             // 
             listView1.Activation = ItemActivation.TwoClick;
             listView1.Alignment = ListViewAlignment.Left;
-            listView1.Anchor = AnchorStyles.Top;
+            listView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             listView1.ImeMode = ImeMode.On;
             listView1.LabelEdit = true;
-            listView1.Location = new Point(526, 12);
+            listView1.Location = new Point(441, 12);
             listView1.MultiSelect = false;
             listView1.Name = "listView1";
-            listView1.Size = new Size(317, 539);
+            listView1.Size = new Size(331, 539);
             listView1.Sorting = SortOrder.Ascending;
             listView1.TabIndex = 1;
             listView1.UseCompatibleStateImageBehavior = false;
             listView1.View = View.List;
-            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged;
             listView1.DoubleClick += listView1_DoubleClick;
-            // 
-            // richTextBox1
-            // 
-            richTextBox1.Location = new Point(896, 12);
-            richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(419, 539);
-            richTextBox1.TabIndex = 9;
-            richTextBox1.Text = "";
-            richTextBox1.TextChanged += richTextBox1_TextChanged;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1352, 607);
-            Controls.Add(richTextBox1);
+            AutoSize = true;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            ClientSize = new Size(784, 561);
             Controls.Add(listView1);
             Controls.Add(treeView1);
             DoubleBuffered = true;
@@ -90,7 +80,6 @@
 
         private TreeView treeView1;
         private ListView listView1;
-        private RichTextBox richTextBox1;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.ComponentModel.BackgroundWorker backgroundWorker2;
         private System.ComponentModel.BackgroundWorker backgroundWorker3;
--- projekt na ppW/Form1.cs
+++ projekt na ppW/Form1.cs
@@ -3,9 +3,6 @@ using System.IO;
 
 namespace projekt_na_ppW
 {
-
-
-
     public partial class Form1 : Form
     {
         private string saver { get; set; }
@@ -13,15 +10,11 @@ namespace projekt_na_ppW
         {
             get { return saver; }
             set
-
             {
-                richTextBox1.Text = value;
                 saver = value;
                 acctListView(this);
             }
         }
-        public List<string> truePathsOfFile { get; set; } = new List<string>();
-        public List<string> truePathsOfDir { get; set; } = new List<string>();
         public Form1()
         {
             InitializeComponent();
@@ -31,93 +24,66 @@ namespace projekt_na_ppW
             TreeNode node = treeView1.Nodes[0];
             foreach (string path in pog)
             {
-                node.Nodes.Add(path,path.Substring(path.LastIndexOf('\\') + 1));
-                truePathsOfDir.Add(path);
+                node.Nodes.Add(path, path.Substring(path.LastIndexOf('\\') + 1));
             }
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void acctListView(object sender)
         {
             listView1.Items.Clear();
-            truePathsOfFile.Clear();
             try
             {
                 var txtFiles = Directory.EnumerateFiles(currentFolder, "*");
                 foreach (var f in txtFiles)
                 {
-                    listView1.Items.Add(f.Substring(f.LastIndexOf('\\') + 1));
-                    truePathsOfFile.Add(f);
+                    listView1.Items.Add( f.Substring(f.LastIndexOf('\\') + 1), f);
                 }
             }
             catch (Exception ex)
             {
-
             }
         }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
         {
-
-            truePathsOfFile.Clear();
-            truePathsOfDir.Clear();
             string path = "";
             TreeNode actNode = e.Node;
-            currentFolder= actNode.Name;
+            currentFolder = actNode.Name;
             try
             {
                 var txtFiles = Directory.GetDirectories(currentFolder);
                 foreach (var f in txtFiles)
                 {
 
-                    actNode.Nodes.Add(f,f.Substring(f.LastIndexOf('\\') + 1));
-                    truePathsOfDir.Add(f);
+                    actNode.Nodes.Add(f, f.Substring(f.LastIndexOf('\\') + 1));
                 }
             }
             catch (Exception ex)
             {
-
             }
-
-
-        }
-
-        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
-        {
         }
 
         private void listView1_DoubleClick(object sender, EventArgs e)
         {
-            string path = truePathsOfFile[listView1.SelectedItems[0].Index];
-            string att = path.Substring(path.LastIndexOf('.') + 1);
-            if (att == "txt" || att == "cpp" || att == "csv" || att == "c" || att == "py" || att == "cs") // jakieœ bazowe rozszerzenia plików textowych nie wiem jak to zrobiæ ¿eby samo czyta³o i sprawdza³o czy dany kod jest mo¿liwy do edycji textowej
+            try
             {
-                formularzZapisu window = new formularzZapisu(path);
-                window.Show();
-                return;
-                
-            }
-            using (Process proc = new Process())
-            {
-                proc.StartInfo.FileName =  (truePathsOfFile[listView1.SelectedItems[0].Index]);//("notepad.exe");
-                proc.StartInfo.UseShellExecute = true;
-                proc.StartInfo.RedirectStandardOutput = false;
-                proc.Start();
-            }
-        }
+                string path = listView1.Items[listView1.SelectedItems[0].Index].ImageKey;
+                string att = path.Substring(path.LastIndexOf('.') + 1);
+                if (att == "txt" || att == "cpp" || att == "csv" || att == "c" || att == "py" || att == "cs") // jakieœ bazowe rozszerzenia plików textowych nie wiem jak to zrobiæ ¿eby samo czyta³o i sprawdza³o czy dany kod jest mo¿liwy do edycji textowej
+                {
+                    formularzZapisu window = new formularzZapisu(path);
+                    window.Show();
+                    return;
 
-        private void richTextBox1_TextChanged(object sender, EventArgs e)
-        {
-
+                }
+                using (Process proc = new Process())
+                {
+                    proc.StartInfo.FileName = (listView1.Items[listView1.SelectedItems[0].Index].ImageKey);//("notepad.exe");
+                    proc.StartInfo.UseShellExecute = true;
+                    proc.StartInfo.RedirectStandardOutput = false;
+                    proc.Start();
+                }
+            }
+            catch { }
         }
     }
 }
